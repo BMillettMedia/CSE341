91bfbbec2fd579ce1d872513102d6ed965f5edff
Bryan Millett <bryanmillett@cox.net>
2025-09-15 14:17:55 -0700

    render fix?

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express();
- 
+
+app.use(helmet());
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send("Hal, open the pod bay doors.");
 });
- 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+
+app.get('/health', (req, res) => {
+  res.send('OK');
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Web Server is listening at port ' + PORT);
+});
 //code to say Hello
