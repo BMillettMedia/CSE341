d9b5d13b2911621adc56e206eb0bfc34e6b43c2b
Bryan Millett <bryanmillett@cox.net>
2025-10-02 14:16:46 -0700

    L3-personal_QuickStart Test

---
+++ lesson3-http/server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const contactsRoutes = require('./routes/contacts');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Use the contacts routes
+app.use('/contacts', contactsRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+//professor solution?
+// app.use('/api/contacts', contactsRoutes);
