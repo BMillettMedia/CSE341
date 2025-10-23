587fc4e5032f9d86d5b667a528d126d679b684be
Bryan Millett <bryanmillett@cox.net>
2025-10-22 20:57:39 -0700

    l5-6 corrections & lesson start

---
+++ lesson5-alt/swagger.js
@@ -0,0 +1,32 @@
+// swagger.js
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Student Game Library API',
+    description: 'API for managing and viewing a collection of student favorite games.',
+    version: '1.0.0'
+  },
+  host: 'localhost:3000', // will update for Render later
+  schemes: ['http'],
+  basePath: '/',
+  definitions: {
+    Game: {
+      _id: '0001',
+      title: 'Hollow Knight',
+      platform: 'PC',
+      genre: 'Metroidvania',
+      rating: 'E10+',
+      releaseDate: '2017-02-26',
+      developer: 'Team Cherry',
+      coverArt: 'https://upload.wikimedia.org/hollow_knight.jpg'
+    }
+  }
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./app.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log('✅ Swagger documentation generated.');
+});
