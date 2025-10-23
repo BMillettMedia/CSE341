4795e743e772d00959645abb20718baf96c8e896
Bryan Millett <bryanmillett@cox.net>
2025-10-22 21:09:13 -0700

    reminder comment

--- lesson5-alt/routes/index.js
+++ lesson5-alt/routes/index.js
@@ -5,3 +5,4 @@ router.use('/games', require('./games'));
 router.use('/wishlist', require('./wishlist'));
 
 module.exports = router;
+//index.js from lesson5-alt/routes double router for different paths of data
